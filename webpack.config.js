2343abcdbba46bd3cb4df3f5be1ec586d9a92842
twall <tomwall.col@gmail.com>
2017-02-10 18:13:22 -0800

    new repo for react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/main.jsx',
+      Nav: 'app/components/nav.jsx'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react','es2015']
+        },
+        test: /\.jsx?$/ ,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+}
